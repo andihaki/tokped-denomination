d056ef2e0d8c281601241196fe830de236f5aa1b
andi hakim arif <andi.hakim.arif@gmail.com>
2019-07-08 19:05:34 +0700

    hmm

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,14 @@
 import React, { useState } from "react";
 import "./App.css";
+import Results from "./Results";
 
 const App: React.FC = () => {
-  const fractions = [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50];
-  const [money, setMoney] = useState(50000);
-  let results = "";
-
-  const countIt = (counter: number, value: number, fraction: number) => {
-    // console.log(counter, value, fraction);
-    if (value > fraction) {
-      counter += 1;
-      countIt(counter, value - fraction, fraction);
-    } else {
-      // console.log(value, `${counter} x Rp${fraction}, `);
-      results += `${counter} x Rp${fraction}, `;
-      // counter = 0;
-    }
-    return counter;
-  };
+  const [money, setMoney] = useState(0);
+  const [renderResults, setRenderResults] = useState(false);
 
   const submitHandler = (e: any) => {
-    console.log("ok", money);
     e.preventDefault();
-
-    let _money = money;
-    // let _money = 50000;
-
-    fractions.forEach(fraction => {
-      // console.log(money, fraction);
-      let counter = 0;
-      if (_money > fraction) {
-        counter = countIt(counter, _money, fraction);
-
-        _money = _money - counter * fraction;
-        console.log(counter, _money, fraction);
-        counter = 0;
-      }
-    });
-
-    console.log(results);
+    setRenderResults(!renderResults);
   };
   return (
     <div className="App">
@@ -48,10 +18,14 @@ const App: React.FC = () => {
           <input
             type="string"
             placeholder="input amount of money"
-            onChange={e => setMoney(parseInt(e.target.value, 0))}
+            onChange={e => {
+              setMoney(parseInt(e.target.value, 0));
+              setRenderResults(false);
+            }}
           />
           <button>Submit</button>
         </form>
+        {renderResults && <Results value={money} />}
       </header>
     </div>
   );
